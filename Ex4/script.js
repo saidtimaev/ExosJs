e69e089769ea072626862b18fbcdff5574fab1e9
saidtimaev <said.timaev@hotmail.com>
2024-02-12 11:38:56 +0100

    ajout commentaires

--- Ex4/script.js
+++ Ex4/script.js
@@ -5,8 +5,9 @@ const carre = document.createElement('div')
 
 
 
-
+// Ajoute class .carres à l'element carre
 carre.classList.add('carres');
+// Ajoute des propriétés à la classe .carres de l'element carre
 carre.style.display = 'inline-block';
 carre.style.verticalAlign = 'top';
 carre.style.height = '60px';
@@ -22,6 +23,7 @@ document.addEventListener('keydown', function(event) {
 
         let newCarre = carre.cloneNode();
 
+        // Ajoute des carrés à l'élément div .container
         container.append(newCarre);
         carre.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     }
@@ -32,7 +34,7 @@ document.addEventListener('keydown', function(event) {
     // Si on appui sur la touche HAUT
     if (event.key === 'ArrowUp') {
 
-
+        // Supprime le dernier carré crée
         container.lastChild.remove();
     }
 });
