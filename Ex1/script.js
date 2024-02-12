ce165135b704903174c1a06df0cedd25a99ea94f
saidtimaev <said.timaev@hotmail.com>
2024-02-12 13:35:35 +0100

    ajout commentaires

--- Ex1/script.js
+++ Ex1/script.js
@@ -5,11 +5,17 @@ forme.addEventListener('click', afficherAlerte);
 
 
 
+// Fonction qui va afficher une alerte avec les toutes les propriétés et leurs valeurs de la classe .forme
 function afficherAlerte(){
 
+    // Crée un élement forme à partir de .forme <div>
     const forme = document.querySelector('.forme');
+    console.log(forme);
+    // Renvoie un objet qui contient toutes les propriétés style de .forme
     const style = getComputedStyle(forme)
-    const backgroundColor = style.backgroundColor;
+    console.log(style)
+    // On définit différentes propriétés à .forme
+    const backgroundColor = forme.style.backgroundColor;
     const color = style.color;
     const height = style.height;
     const width = style.width;
@@ -17,6 +23,7 @@ function afficherAlerte(){
     const fontFamily = style.fontFamily;
     const fontSize = style.fontSize;
 
+    //Affiche une alerte avec les propriétés style de forme et le valeurs
     alert('Class : ' + forme.className + 
             '\n - Background color : ' + backgroundColor + 
             '\n - Color : ' + color +
